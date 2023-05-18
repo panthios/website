eaff86d1f68d7e1b5f7cde85a4a618f271aafc5f
Carlos Kieliszewski <carlos.kieliszewski@gmail.com>
2023-05-18 04:57:33 -0600

    Added first project docs

--- src/content/config.ts
+++ src/content/config.ts
@@ -7,6 +7,20 @@ const referenceCollection = defineCollection({
   })
 });
 
+const projectCollection = defineCollection({
+  schema: z.object({
+    name: z.string(),
+    stability: z.enum([
+      'concept',
+      'alpha',
+      'beta',
+      'stable',
+      'deprecated'
+    ])
+  })
+});
+
 export const collections = {
-  reference: referenceCollection
+  reference: referenceCollection,
+  project: projectCollection
 }
---
+++ src/content/project/barley.md
@@ -0,0 +1,6 @@
+---
+name: Barley
+stability: concept
+---
+
+Barley is a simple and fast workflow engine written in Rust. It creates scripts for any desired task, using relationships between commands to efficiently run things in parallel. It compiles directly to machine code, so it's fast and easy to distribute.
